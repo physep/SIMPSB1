074864fd481fdcaca2ffbeacc1daec706263ef73
APRENDIZ <APRENDIZ@BOGDFPRDC8F549.FormacionSena.RED>
2019-09-13 13:36:59 -0500

    EDITAR AGENDAR CITA

--- src/java/simpsb/controller/CitasController.java
+++ src/java/simpsb/controller/CitasController.java
@@ -7,21 +7,72 @@ import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import simpsb.dao.CitasFacadeLocal;
-import simpsb.entidades.Citas;
+import simpsb.DAO.EmpleadoFacadeLocal;
+import simpsb.DAO.ServiciosFacadeLocal;
+import simpsb.dao.*;
+import simpsb.entidades.*;
 
 @Named
 @RequestScoped
+
 public class CitasController {
     @EJB
     private CitasFacadeLocal citasFacadeLocal;
     private Citas citas;
     
+    @EJB 
+    private ServiciosFacadeLocal serviciosFacadeLocal;
+    private Servicios servicios;
+    private List<Servicios> listServicios;
+    
+    @EJB 
+    private EmpleadoFacadeLocal empleadoFacadeLocal;
+    private Empleado empleado;
+    private List<Empleado> listEmpleados;
+    
+    
     @PostConstruct
     public void init(){
         citas = new Citas();
+        servicios = new Servicios();
+        listServicios = serviciosFacadeLocal.findAll();
+        empleado = new Empleado();
+        listEmpleados = empleadoFacadeLocal.findAll();
     }
 
+    public Servicios getServicios() {
+        return servicios;
+    }
+
+    public void setServicios(Servicios servicios) {
+        this.servicios = servicios;
+    }
+
+    public List<Servicios> getListServicios() {
+        return listServicios;
+    }
+
+    public void setListServicios(List<Servicios> listServicios) {
+        this.listServicios = listServicios;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+
+    public List<Empleado> getListEmpleados() {
+        return listEmpleados;
+    }
+
+    public void setListEmpleados(List<Empleado> listEmpleados) {
+        this.listEmpleados = listEmpleados;
+    }
+    
+    
     public Citas getCitas() {
         return citas;
     }

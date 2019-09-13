e870a9afd9f53dd3193168edf66d584c09a0407b
APRENDIZ <APRENDIZ@BOGDFPRDC8F546.FormacionSena.RED>
2019-09-13 17:54:53 -0500

    13/09/19 Horarios 5:54

---
+++ src/java/simpsb/controller/HorarioControlller.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simpsb.controller;
+
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import simpsb.dao.*;
+import simpsb.entidades.*;
+
+@Named
+@RequestScoped
+public class HorarioControlller {
+    
+}
--- src/java/simpsb/controller/HorariosController.java
+++ src/java/simpsb/controller/HorariosController.java
@@ -7,24 +7,25 @@ package simpsb.controller;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.inject.Named;
 import simpsb.dao.HorariotrabajoFacadeLocal;
 import simpsb.entidades.Horariotrabajo;
 
-/**
- *
- * @author APRENDIZ
- */
+@Named
+@RequestScoped
 public class HorariosController {
     @EJB
     private HorariotrabajoFacadeLocal HorariotrabajoFacadeLocal;
-    private Horariotrabajo Horariotrabajo;@PostConstruct
+    private Horariotrabajo Horariotrabajo;
     
     @PostConstruct
     public void init(){
          Horariotrabajo = new Horariotrabajo();
     }
+    
 
 
     public HorariotrabajoFacadeLocal getHorariotrabajoFacadeLocal() {

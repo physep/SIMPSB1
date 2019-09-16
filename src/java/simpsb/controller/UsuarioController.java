264128e040e662eddaca3494c91db3baa37bf493
APRENDIZ <APRENDIZ@BOGDFPRDC10F039.FormacionSena.RED>
2019-09-16 17:05:41 -0500

    Normalizacion Registro

---
+++ src/java/simpsb/controller/UsuarioController.java
@@ -0,0 +1,42 @@
+package simpsb.controller;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import simpsb.dao.UsuarioFacadeLocal;
+import simpsb.entidades.Usuario;
+
+
+@Named
+@RequestScoped
+public class UsuarioController {
+    
+    @EJB 
+    private UsuarioFacadeLocal usuarioFacadeLocal;
+    private Usuario usuario;
+   
+    @PostConstruct
+    public void init(){
+        usuario = new Usuario();
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+    public void registrarUsuario(){
+        try {
+            usuarioFacadeLocal.create(usuario);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Aviso", "Se ha registrado exitosamente"));
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Error", "Ha ocurrido un error al registrarse"));
+        }
+    }
+}
